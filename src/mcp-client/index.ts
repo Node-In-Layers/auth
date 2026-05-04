ea903eee29135945aeb1b6b350d1e1f3822eda1b
Mike Cornwell <mike@mikecornwell.com>
2026-05-04 08:44:18 -0400

    feat(export): export createClient directly

--- src/mcp-client/index.ts
+++ src/mcp-client/index.ts
@@ -3,4 +3,5 @@ import { AuthNamespace } from '../types.js'
 export const name = AuthNamespace.McpClient
 
 export * as entries from './entries.js'
+export { createClient } from './entries.js'
 export * as types from './types.js'
