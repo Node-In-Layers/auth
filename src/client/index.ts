6976df8e7b12f3bb0eee6bfe7850511565f5eec0
Mike Cornwell <mike@mikecornwell.com>
2026-05-06 11:26:05 -0400

    fix(name): fix namespace

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,4 +1,6 @@
+import { AuthNamespace } from '../types.js'
+
 export * as services from './services.js'
 export * as features from './features.js'
 export * as types from './types.js'
-export const name = 'client'
+export const name = AuthNamespace.Client
